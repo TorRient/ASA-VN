0a0a8988565966a4828940ce1488d887902a6ae8
torrient <hoangthuyen1998@gmail.com>
2020-05-11 21:48:15 +0700

    First

---
+++ inference.py
@@ -0,0 +1,75 @@
+from train import make_aspect_category_model
+import numpy as np
+import pickle
+import os
+import spacy
+import re
+import torch
+from src.module.utils.constants import UNK
+
+url = re.compile('(<url>.*</url>)')
+spacy_en = spacy.load('en')
+
+import yaml
+
+config = yaml.safe_load(open('config.yml'))
+# mode = config['mode']
+# os.environ["CUDA_VISIBLE_DEVICES"] = str(config['aspect_' + mode + '_model'][config['aspect_' + mode + '_model']['type']]['gpu'])
+
+def check(x):
+    return len(x) >= 1 and not x.isspace()
+
+def tokenizer(text):
+    tokens = [tok.text for tok in spacy_en.tokenizer(url.sub('@URL@', text))]
+    return list(filter(check, tokens))
+
+with open(os.path.join(config['base_path'], 'processed/index2word.pickle'), 'rb') as handle:
+    index2word = pickle.load(handle)
+
+with open(os.path.join(config['base_path'], 'processed/word2index.pickle'), 'rb') as handle:
+    word2index = pickle.load(handle)
+
+def make_data(sen, aspt , word2index):
+    data = [sen + "__split__" + aspt]
+    sentence = []
+    aspect = []
+    f = lambda x: word2index[x] if x in word2index else word2index[UNK]
+    g = lambda x: list(map(f, tokenizer(x)))
+    cd = {
+        'FOOD#STYLEOPTIONS': 0,
+        'FOOD#QUALITY': 1,
+        'AMBIENCE#GENERAL': 2,
+        'RESTAURANT#GENERAL': 3,
+        'SERVICE#GENERAL': 4,
+        'FOOD#PRICES': 5,
+        'RESTAURANT#PRICES': 6,
+        'LOCATION#GENERAL': 7,
+        'RESTAURANT#MISCELLANEOUS': 8,
+        'DRINKS#STYLEOPTIONS': 9,
+        'DRINKS#PRICES': 10,
+        'DRINKS#QUALITY': 11
+    }
+    for piece in data:
+        text, category= piece.split('__split__')
+        sentence.append(g(text))
+        aspect.append(cd[category])
+    max_length = lambda x: max([len(y) for y in x])
+    sentence_max_len = max_length(sentence)
+    num = len(data)
+    for i in range(num):
+        sentence[i].extend([0] * (sentence_max_len - len(sentence[i])))
+    sentence = np.asarray(sentence, dtype=np.int32)
+    aspect = np.asarray(aspect, dtype=np.int32)
+    return torch.tensor(sentence).long(), torch.tensor(aspect).long()
+
+# def make_tensor()
+
+sen, aspect = make_data("view đẹp, dịch vụ chán", 'SERVICE#GENERAL', word2index)
+
+model = make_aspect_category_model.make_model(config)
+model = model.cuda()
+model_path = os.path.join(config['base_path'], 'checkpoints/%s.pth' % config['aspect_' + mode + '_model']['type'])
+model.load_state_dict(torch.load(model_path))
+
+print(model(sen, aspect))
+
---
+++ prepare_datavlsp.py
@@ -0,0 +1,72 @@
+import argparse
+from tqdm import tqdm
+from pyvi import ViTokenizer
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--mode", default="restaurant", help="mode restaurant or hotel")
+
+args = parser.parse_args()
+
+if args.mode == "restaurant":
+    train = './VLSP2018/1-VLSP2018-SA-Restaurant-train.txt'
+    dev = './VLSP2018/2-VLSP2018-SA-Restaurant-dev.txt'
+    test = './VLSP2018/3-VLSP2018-SA-Restaurant-test.txt'
+
+else:
+    train = './VLSP2018/1-VLSP2018-SA-Hotel-train.txt'
+    dev = './VLSP2018/2-VLSP2018-SA-Hotel-dev.txt'
+    test = './VLSP2018/3-VLSP2018-SA-Hotel-test.txt'
+
+train_out = './VLSP2018/' + 'train.xml'
+dev_out = './VLSP2018/' + 'val.xml'
+test_out = './VLSP2018/' + 'test.xml'
+
+def write_xml(path_txt, output):
+    sentences = []
+    label = []
+    poli = []
+    count = 0
+    with open(path_txt, 'r') as files:
+        for line in files:
+            sentences.append(line)
+    print(len(sentences))
+    with open(output, 'w') as out:
+        out.write('<?xml version="1.0" encoding="utf-8"?>')
+        out.write('<sentences>')
+
+        for idx in tqdm(range(int(len(sentences)/4) + 1)):
+            start = '#' + str(idx+1)
+            for line in sentences:
+                if start in line:
+                    token = ViTokenizer.tokenize(str(sentences[idx*4+1]))
+                    if len(token.split(' ')) > 512:
+                        count += 1
+                        continue
+                    out.write('<sentence>')
+                    out.write('<text>')
+                    token = token.replace("&", 'và').replace("<", '&lt;').replace(">", '&gt;')
+                    out.write(token)
+                    out.write('</text>')
+                    out.write('<aspectCategories>')
+                    # print(sentences[idx+1])
+                    for aspect in sentences[idx*4+2].split('}, '):
+                        category, polarity = aspect.split(',')
+                        category = category.replace('{','').replace(" ", '').replace("&", '')
+                        polarity = polarity.replace('}','').replace(" ", '').replace("\n", '')
+                        if category not in label:
+                            label.append(category)
+                        if polarity not in poli:
+                            poli.append(polarity)
+                        out.write('<aspectCategory category="{}" polarity="{}"/>'.format(category, polarity))
+                    out.write('</aspectCategories>')
+                    out.write('</sentence>')
+                    break
+        out.write('</sentences>')
+    return label, poli, count
+label, poli, count = write_xml(train, train_out)
+write_xml(dev, dev_out)
+write_xml(test, test_out)
+print(label)
+print(poli)
+print(count)
