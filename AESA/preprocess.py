5d1807310b983d0c3f800369225d6bdedac46a12
cuonghp2112 <cuonghp2112@gmail.com>
2020-06-08 01:53:32 +0700

    update aspect extraction model

---
+++ AESA/preprocess.py
@@ -0,0 +1,143 @@
+import re
+import string
+import unicodedata as ud
+
+def normalize_text(vncorenlp,text,STOP_WORDS,n_min,n_max):
+    text = ud.normalize('NFC', text)
+    #Remove các ký tự kéo dài: vd: đẹppppppp
+    text = re.sub(r'([A-Z])\1+', lambda m: m.group(1).upper(), text, flags=re.IGNORECASE)
+
+
+    #Chuẩn hóa tiếng Việt, xử lý emoj, chuẩn hóa tiếng Anh, thuật ngữ
+    replace_list = {
+        'òa': 'oà', 'óa': 'oá', 'ỏa': 'oả', 'õa': 'oã', 'ọa': 'oạ', 'òe': 'oè', 'óe': 'oé','ỏe': 'oẻ',
+        'õe': 'oẽ', 'ọe': 'oẹ', 'ùy': 'uỳ', 'úy': 'uý', 'ủy': 'uỷ', 'ũy': 'uỹ','ụy': 'uỵ', 'uả': 'ủa',
+        'ả': 'ả', 'ố': 'ố', 'u´': 'ố','ỗ': 'ỗ', 'ồ': 'ồ', 'ổ': 'ổ', 'ấ': 'ấ', 'ẫ': 'ẫ', 'ẩ': 'ẩ',
+        'ầ': 'ầ', 'ỏ': 'ỏ', 'ề': 'ề','ễ': 'ễ', 'ắ': 'ắ', 'ủ': 'ủ', 'ế': 'ế', 'ở': 'ở', 'ỉ': 'ỉ',
+        'ẻ': 'ẻ', 'àk': u' à ','aˋ': 'à', 'iˋ': 'ì', 'ă´': 'ắ','ử': 'ử', 'e˜': 'ẽ', 'y˜': 'ỹ', 'a´': 'á',
+        #Quy các icon về 2 loại emoj: Tích cực hoặc tiêu cực
+        "👹": " positive ", "👻": " positive ", "💃": " positive ",'🤙': ' positive ', '👍': ' positive ',
+        "💄": " positive ", "💎": " positive ", "💩": " positive ","😕": " nagative ", "😱": " nagative ", "😸": "positive",
+        "😾": " nagative ", "🚫": " nagative ",  "🤬": " nagative ","🧚": " positive ", "🧡": " positive ",'🐶':' positive ',
+        '👎': ' nagative ', '😣': ' nagative ','✨': ' positive ', '❣': ' positive ','☀': ' positive ',':D': ' positive ',
+        '♥': ' positive ', '🤩': ' positive ', 'like': ' positive ', '💌': ' positive ',
+        '🤣': ' positive ', '🖤': ' positive ', '🤤': ' positive ', ':(': ' nagative ', '😢': ' nagative ',':\'(': ' nagative ',
+        '❤': ' positive ', '😍': ' positive ', '😘': ' positive ', '😪': ' nagative ', '😊': ' positive ',
+        '?': ' ? ', '😁': ' positive ', '💖': ' positive ', '😟': ' nagative ', '😭': ' nagative ',
+        '💯': ' positive ', '💗': ' positive ', '♡': ' positive ', '💜': ' positive ', '🤗': ' positive ',
+        '^^': ' positive ', '😨': ' nagative ', '☺': ' positive ', '💋': ' positive ', '👌': ' positive ',
+        '😖': ' nagative ', '😀': ' positive ', ':((': ' nagative ', '😡': ' nagative ', '😠': ' nagative ',
+        '😒': ' nagative ', '🙂': ' positive ', '😏': ' nagative ', '😝': ' positive ', '😄': ' positive ',
+        '😙': ' positive ', '😤': ' nagative ', '😎': ' positive ', '😆': ' positive ', '💚': ' positive ',
+        '✌': ' positive ', '💕': ' positive ', '😞': ' nagative ', '😓': ' nagative ', '️🆗️': ' positive ',
+        '😉': ' positive ', '😂': ' positive ', ':v': '  positive ', '=))': '  positive ', '😋': ' positive ',
+        '💓': ' positive ', '😐': ' nagative ', ':3': ' positive ', '😫': ' nagative ', '😥': ' nagative ',
+        '😃': ' positive ', '😬': ' 😬 ', '😌': ' 😌 ', '💛': ' positive ', '🤝': ' positive ', '🎈': ' positive ',
+        '😗': ' positive ', '🤔': ' nagative ', '😑': ' nagative ', '🔥': ' nagative ', '🙏': ' nagative ',
+        '🆗': ' positive ', '😻': ' positive ', '💙': ' positive ', '💟': ' positive ',
+        '😚': ' positive ', '❌': ' nagative ', '👏': ' positive ', ';)': ' positive ', '<3': ' positive ',
+        '🌝': ' positive ',  '🌷': ' positive ', '🌸': ' positive ', '🌺': ' positive ',
+        '🌼': ' positive ', '🍓': ' positive ', '🐅': ' positive ', '🐾': ' positive ', '👉': ' positive ',
+        '💐': ' positive ', '💞': ' positive ', '💥': ' positive ', '💪': ' positive ',
+        '💰': ' positive ',  '😇': ' positive ', '😛': ' positive ', '😜': ' positive ',
+        '🙃': ' positive ', '🤑': ' positive ', '🤪': ' positive ','☹': ' nagative ',  '💀': ' nagative ',
+        '😔': ' nagative ', '😧': ' nagative ', '😩': ' nagative ', '😰': ' nagative ', '😳': ' nagative ',
+        '😵': ' nagative ', '😶': ' nagative ', '🙁': ' nagative ','👍🏻': ' positive ',
+        '🍤': '', '😬': '', '🔹': '','🙆🏼': '', '🍛': '', '🍗': '','=))': '  positive ','=)))': '  positive ',
+        '🚹': '', '🐟': '', '🍮': '','😅': '', '💔': '', '🙋': '','☔':'','✔':'','🤘':'',
+        '🔵': '', '👇': '', '😽': '','🍡': '', '🍢': '', '✅': '','▶':'','•':'','♻':'',
+        '🍣': '', '🍲': '', '🍜': '','🙀': '', '🌋': '', '🎊': '','ㅁ':'','ㅠㅠ':'',
+        '⛔': '', '🦀': '', '📝': '','🏠': '', '🐷': '', '🙄': '',
+        '🎀': '', '💘': '', '☕': '',
+        #Chuẩn hóa 1 số sentiment words/English words
+        ':))': '  positive ', ':)': ' positive ', 'ô kêi': ' ok ', 'okie': ' ok ', ' o kê ': ' ok ',
+        'okey': ' ok ', 'ôkê': ' ok ', 'oki': ' ok ', ' oke ':  ' ok ',' okay':' ok ','okê':' ok ',
+        ' tks ': u' cám ơn ', 'thks': u' cám ơn ', 'thanks': u' cám ơn ', 'ths': u' cám ơn ', 'thank': u' cám ơn ',
+        '⭐': 'star ', '*': 'star ', '🌟': 'star ', '🎉': u' positive ',
+        'kg ': u' không ','not': u' không ', u' kg ': u' không ', '"k ': u' không ',' kh ':u' không ','kô':u' không ','hok':u' không ',' kp ': u' không phải ',u' kô ': u' không ', '"ko ': u' không ', u' ko ': u' không ', u' k ': u' không ', 'khong': u' không ', u' hok ': u' không ',
+        'he he': ' positive ','hehe': ' positive ','hihi': ' positive ', 'haha': ' positive ', 'hjhj': ' positive ',
+        ' lol ': ' nagative ',' cc ': ' nagative ','cute': u' dễ thương ','huhu': ' nagative ', ' vs ': u' với ', 'wa': ' quá ', 'wá': u' quá', 'j': u' gì ', '“': ' ',
+        ' sz ': u' cỡ ', 'size': u' cỡ ', u' đx ': u' được ', 'dk': u' được ', 'dc': u' được ', 'đk': u' được ',
+        'đc': u' được ','authentic': u' chuẩn chính hãng ',u' aut ': u' chuẩn chính hãng ', u' auth ': u' chuẩn chính hãng ', 'thick': u' positive ', 'store': u' cửa hàng ',
+        'shop': u' cửa hàng ', 'sp': u' sản phẩm ', 'gud': u' tốt ','god': u' tốt ','wel done':' tốt ', 'good': u' tốt ', 'gút': u' tốt ',
+        'sấu': u' xấu ','gut': u' tốt ', u' tot ': u' tốt ', u' nice ': u' tốt ', 'perfect': 'rất tốt', 'bt': u' bình thường ',
+        'time': u' thời gian ', 'qá': u' quá ', u' ship ': u' giao hàng ', u' m ': u' mình ', u' mik ': u' mình ',
+        'ể': 'ể', 'product': 'sản phẩm', 'quality': 'chất lượng','chat':' chất ', 'excelent': 'hoàn hảo', 'bad': 'tệ','fresh': ' tươi ','sad': ' tệ ',
+        'date': u' hạn sử dụng ', 'hsd': u' hạn sử dụng ','quickly': u' nhanh ', 'quick': u' nhanh ','fast': u' nhanh ','delivery': u' giao hàng ',u' síp ': u' giao hàng ',
+        'beautiful': u' đẹp tuyệt vời ', u' tl ': u' trả lời ', u' r ': u' rồi ', u' shopE ': u' cửa hàng ',u' order ': u' đặt hàng ',
+        'chất lg': u' chất lượng ',u' sd ': u' sử dụng ',u' dt ': u' điện thoại ',u' nt ': u' nhắn tin ',u' tl ': u' trả lời ',u' sài ': u' xài ',u'bjo':u' bao giờ ',
+        'thik': u' thích ',u' sop ': u' cửa hàng ', ' fb ': ' facebook ', ' face ': ' facebook ', ' very ': u' rất ',u'quả ng ':u' quảng  ',
+        'dep': u' đẹp ',u' xau ': u' xấu ','delicious': u' ngon ', u'hàg': u' hàng ', u'qủa': u' quả ',
+        'iu': u' yêu ','fake': u' giả mạo ', 'trl': 'trả lời', '><': u' positive ',
+        ' por ': u' tệ ',' poor ': u' tệ ', 'ib':u' nhắn tin ', 'rep':u' trả lời ',u'fback':' feedback ','fedback':' feedback ',
+        #dưới 3* quy về 1*, trên 3* quy về 5*
+        '6 sao': ' 5star ','6 star': ' 5star ', '5star': ' 5star ','5 sao': ' 5star ','5sao': ' 5star ',
+        'starstarstarstarstar': ' 5star ', '1 sao': ' 1star ', '1sao': ' 1star ','2 sao':' 1star ','2sao':' 1star ',
+        '2 starstar':' 1star ','1star': ' 1star ', '0 sao': ' 1star ', '0star': ' 1star ',}
+
+    for k, v in replace_list.items():
+        text = text.replace(k, v)
+    
+    upper_char = r"[AĂÂÁẮẤÀẰẦẢẲẨÃẴẪẠẶẬĐEÊÉẾÈỀẺỂẼỄẸỆIÍÌỈĨỊOÔƠÓỐỚÒỒỜỎỔỞÕỖỠỌỘỢUƯÚỨÙỪỦỬŨỮỤỰYÝỲỶỸỴAĂÂÁẮẤÀẰẦẢẲẨÃẴẪẠẶẬĐEÊÉẾÈỀẺỂẼỄẸỆIÍÌỈĨỊOÔƠÓỐỚÒỒỜỎỔỞÕỖỠỌỘỢUƯÚỨÙỪỦỬŨỮỤỰYÝỲỶỸỴAĂÂÁẮẤÀẰẦẢẲẨÃẴẪẠẶẬĐEÊÉẾÈỀẺỂẼỄẸỆIÍÌỈĨỊOÔƠÓỐỚÒỒỜỎỔỞÕỖỠỌỘỢUƯÚỨÙỪỦỬŨỮỤỰYÝỲỶỸỴAĂÂÁẮẤÀẰẦẢẲẨÃẴẪẠẶẬĐEÊÉẾÈỀẺỂẼỄẸỆIÍÌỈĨỊOÔƠÓỐỚÒỒỜỎỔỞÕỖỠỌỘỢUƯÚỨÙỪỦỬŨỮỤỰYÝỲỶỸỴAĂÂÁẮẤÀẰẦẢẲẨÃẴẪẠẶẬĐEÊÉẾÈỀẺỂẼỄẸỆIÍÌỈĨỊOÔƠÓỐỚÒỒỜỎỔỞÕỖỠỌỘỢUƯÚỨÙỪỦỬŨỮỤỰYÝỲỶỸỴAĂÂÁẮẤÀẰẦẢẲẨÃẴẪẠẶẬĐEÊÉẾÈỀẺỂẼỄẸỆIÍÌỈĨỊOÔƠÓỐỚÒỒỜỎỔỞÕỖỠỌỘỢUƯÚỨÙỪỦỬŨỮỤỰYÝỲỶỸỴA-Z]"
+    p_string = r"(\S)(" + upper_char + r")"
+    pattern = re.compile(p_string)
+    text = re.sub(pattern, r"\1 \2", text)
+    text = re.sub(r"(\d+)([^\d\s]+)", r"\1 \2", text)
+    text = re.sub(r"([^\d\s]+)(\d+)", r"\1 \2", text)
+    text = re.sub(r"\d+\s*(k|đ|Đ|d)\d*\s*", r" giá cả ", text)
+    text = re.sub(r"#\S+", r" hagtag ", text)
+    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags=re.UNICODE)
+    text = emoji_pattern.sub(r'', text)
+    # Chuyển thành chữ thường
+    text = text.lower()
+    # text = re.sub(r"\d", r"", text)
+    # chuyen punctuation thành space
+    translator = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
+    text = text.translate(translator)
+
+    sens = vncorenlp.pos_tag(text)
+    tokens = []
+    pos_tags = []
+    for sen in sens:
+      for token in sen:
+        tokens.append(token[0])
+        pos_tags.append(token[1])
+    # danh sách danh từ và tính từ
+    nva_words = []
+    for i in range(len(tokens)):
+      if pos_tags[i] in ["V","A","N","Np","Ny","Nc","Nu"]:
+        nva_words.append(tokens[i])
+    
+    ngrams = get_ngram(tokens,STOP_WORDS,n_min,n_max)
+    ngram_feature = u' '.join(ngrams)
+    postag_feature = u' '.join(pos_tags)
+    nva_words_feature = u' '.join(nva_words)
+
+    text = ngram_feature + " " + postag_feature + " " + nva_words_feature
+    #remove nốt những ký tự thừa thãi
+    text = text.replace(u'"', u' ')
+    text = text.replace(u'️', u'')
+    text = text.replace('🏻','')
+    return text
+
+def get_ngram(tokens,STOP_WORDS,n_min,n_max):
+    L = len(tokens)
+    ngrams = set()
+    for j in range(n_min, n_max + 1)[::-1]:
+        for i in range(L - j + 1):
+            e = tokens[i + j - 1]
+            s = tokens[i]
+            if s in STOP_WORDS\
+            or (not s.replace("_","").isalpha() and len(s)==1)\
+            or (not e.replace("_","").isalpha() and len(e)==1):
+                continue
+            ngram = r'_'.join([token for token in tokens[i:i+j]])
+            if ngram not in ngrams:
+                ngrams.add(ngram)
+    return ngrams
+
